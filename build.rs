2ae079d26e5cf7a11beabf83545408a0a20b14b4
Paul <paul.pacher@gmail.com>
2025-10-18 10:22:25 +0200

    build angepasst

--- build.rs
+++ build.rs
@@ -1,12 +1,17 @@
 use std::path::Path;
 
 fn main() {
-    // unsafe { std::env::set_var("PROTOC", "C:/javadev/tools/protoc/bin/protoc"); }
+    // unsafe {
+    //     std::env::set_var("PROTOC", "C:/javadev/tools/protoc/bin/protoc");
+    // }
     let src = Path::new("./src/");
 
     let mut config = prost_build::Config::new();
-    config.protoc_executable("C:/javadev/tools/protoc/bin/protoc");
+    config.protoc_executable("/opt/tools/protoc/bin/protoc");
     config.btree_map(["."]);
-    prost_build::compile_protos(&["./proto/authentication.proto"], &[src]).expect("Failed to compile protos");
-
+    prost_build::compile_protos(
+        &["/home/paul/projekte/rust/marvels/proto/authentication.proto"],
+        &[src],
+    )
+    .expect("Failed to compile protos");
 }
